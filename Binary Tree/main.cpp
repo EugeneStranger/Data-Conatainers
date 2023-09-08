6dd90bd38f9d3915d351fcb05804207985eaccaf
EugeneStranger <Stranger_1254@mail.ru>
2023-09-08 17:42:53 +0300

    BinaryTree Done

--- Binary Tree/main.cpp
+++ Binary Tree/main.cpp
@@ -70,9 +70,9 @@ public:
 		Clear(Root);
 		Root = nullptr;
 	}
-	int Depth()const
+	int depth()const
 	{
-		return Depth(Root);
+		return depth(Root);
 	}
 	int Sum()const
 	{
@@ -94,6 +94,18 @@ public:
 	{
 		print(Root);
 		cout << endl;
+	}
+	void depth_print(int depth)const
+	{
+		depth_print(Root,depth,	64);
+	}
+	void tree_print()const
+	{
+		tree_print(Root,64);
+	}
+	void balance()
+	{
+		balance(Root);
 	}
 	double Avg()const
 	{
@@ -150,11 +162,11 @@ private:
 		Clear(Root->pRight);
 		delete Root;
 	}
-	int Depth(Element* Root)const
+	int depth(Element* Root)const
 	{
 		if (Root == nullptr) return 0;
-		int l_depth = Depth(Root->pLeft)+1;
-		int r_depth = Depth(Root->pRight) + 1;
+		int l_depth = depth(Root->pLeft)+1;
+		int r_depth = depth(Root->pRight) + 1;
 		return
 			l_depth > r_depth ?	l_depth : r_depth;
 	}
@@ -187,6 +199,55 @@ private:
 		print(Root->pLeft);
 		cout << Root->Data << "\t";
 		print(Root->pRight);
+	}
+	void depth_print(Element* Root, int depth, int width)const
+	{
+		if (Root == nullptr)
+		{
+			if (depth == 0) cout.width(width*pow(2,(this->depth()-depth)/3));
+			cout << "";
+			return;
+		}
+		if (depth == 0)
+		{
+			cout.width(width);
+			cout << Root->Data;
+			cout.width(width);
+			cout << " ";
+		}
+		depth_print(Root->pLeft, depth -1,width);
+		depth_print(Root->pRight,depth - 1,width);
+	}
+	void tree_print(Element* Root,int width, int depth = 0)const
+	{
+		if (Root == nullptr)return;
+		if (depth >= this->depth())return;
+		depth_print(Root, depth, width);
+		cout << endl;
+		cout << endl;
+		tree_print(Root, width / 2, depth + 1);
+	}
+	void balance(Element* Root)
+	{
+		if (Root == nullptr)return;
+		if (abs(Count(Root->pLeft) - Count(Root->pRight)) < 2)return;
+		if (Count(Root->pLeft) < Count(Root->pRight))
+		{
+			if (Root->pLeft)insert(Root->Data, Root->pLeft);
+			else Root->pLeft = new Element(Root->Data);
+			Root->Data = minValue(Root->pRight);
+			erase(minValue(Root->pRight), Root->pRight);
+		}
+		else
+		{
+			if (Root->pRight)insert(Root->Data, Root->pRight);
+			else Root->pRight = new Element(Root->Data);
+			Root->Data = maxValue(Root->pLeft);
+			erase(maxValue(Root->pLeft), Root->pLeft);
+		}
+		balance(Root->pLeft);
+		balance(Root->pRight);
+		balance(Root);
 	}
 
 };
@@ -225,7 +286,8 @@ template<typename T>void measure(const char* message, const Tree& tree, T(Tree::
 }
 //#define BASE_CHECK
 //#define DEPTH_CHECK
-#define PREFORMANCE_CHECK
+//#define PREFORMANCE_CHECK
+#define BALANCE_CHECK
 
 void main()
 {
@@ -301,11 +363,15 @@ void main()
 
 #endif //BASE_CHECK
 #ifdef DEPTH_CHECK
-	Tree tree = { 50,25,75,16,32,64,90,28,29};
-	tree.print();
-	cout << "Глубина дерева: " << tree.Depth() << endl;
-	tree.erase(16);
-	tree.print();
+	Tree tree = { 50,25,75,16,32,64,90,28,29,15,17};
+	//tree.print();
+	//cout << "Глубина дерева: " << tree.depth() << endl;
+	//tree.erase(16);
+	//tree.print();
+	int depth;
+	//cout << "Введите глубину дерева: "; cin >> depth;
+	//tree.depth_print(depth);
+	tree.tree_print();
 #endif //DEPTH_CHECK
 #ifdef PREFORMANCE_CHECK
 	int n;
@@ -322,7 +388,7 @@ void main()
 	measure("Сумма элементов дерева: \t\t", tree, &Tree::Sum);
 	measure("Количество элементов дерева: \t", tree, &Tree::Count);
 	measure("Среднее арифмитическое элементов дерева: ", tree, &Tree::Avg);
-	measure("Глубина дерева: \t\t ", tree, &Tree::Depth);
+	measure("Глубина дерева: \t\t ", tree, &Tree::depth);
 
 	cout << "========================Unique Tree===============================" << endl;
 	UniqueTree u_tree;
@@ -335,8 +401,14 @@ void main()
 	measure("Сумма элементов дерева: \t\t", u_tree, &Tree::Sum);
 	measure("Количество элементов дерева: \t", u_tree, &Tree::Count);
 	measure("Среднее арифмитическое элементов дерева: ", u_tree, &Tree::Avg);
-	measure("Глубина дерева: \t\t ", u_tree, &Tree::Depth);
+	measure("Глубина дерева: \t\t ", u_tree, &Tree::depth);
 	
 #endif // PREFORMANCE_CHECK
+#ifdef BALANCE_CHECK
+	Tree tree = { 89,55,34,21,13,8,5,3 };
+	tree.tree_print();
+	tree.balance();
+	tree.tree_print();
+#endif // BALANCE_CHECK
 
 }
